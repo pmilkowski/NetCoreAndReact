59a08344bf91977fc7932c02425053339cacaf96
Pawel Milkowski <milkowskipaw@gmail.com>
2020-09-16 15:11:59 +0200

    Added DataContext

--- API/Startup.cs
+++ API/Startup.cs
@@ -6,16 +6,19 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistence;
 
 namespace API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private const string DefaultConnectionStringKey = "DefaultConnection";
+        public Startup (IConfiguration configuration)
         {
             Configuration = configuration;
         }
@@ -23,28 +26,32 @@ namespace API
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices (IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddDbContext<DataContext> (options =>
+            {
+                options.UseSqlite (Configuration.GetConnectionString (DefaultConnectionStringKey));
+            });
+            services.AddControllers ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment ())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseDeveloperExceptionPage ();
             }
 
-            app.UseHttpsRedirection();
+            app.UseHttpsRedirection ();
 
-            app.UseRouting();
+            app.UseRouting ();
 
-            app.UseAuthorization();
+            app.UseAuthorization ();
 
-            app.UseEndpoints(endpoints =>
+            app.UseEndpoints (endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllers ();
             });
         }
     }
