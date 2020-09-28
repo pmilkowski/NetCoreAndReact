bf79175715ef7a263052826fa4251b6170c7c88e
Pawel Milkowski <milkowskipaw@gmail.com>
2020-09-28 20:00:50 +0200

    Added using axios when deleting an activity

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect, Fragment } from 'react';
-import axios from 'axios';
-import { Container, List } from 'semantic-ui-react';
-import { IActivity } from '../models/activity';
-import { NavBar } from '../../features/nav/NavBar';
-import { ActivityDashboard } from '../../features/activities/dashboard/ActivityDashboard';
-import agent from '../api/agent';
+import React, { useState, useEffect, Fragment } from "react";
+import axios from "axios";
+import { Container, List } from "semantic-ui-react";
+import { IActivity } from "../models/activity";
+import { NavBar } from "../../features/nav/NavBar";
+import { ActivityDashboard } from "../../features/activities/dashboard/ActivityDashboard";
+import agent from "../api/agent";
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
@@ -36,14 +36,16 @@ const App = () => {
   };
 
   const handleDeleteActivity = (id: string) => {
-    setActivities([...activities.filter((a) => a.id != id)]);
+    agent.Activities.delete(id).then(() => {
+      setActivities([...activities.filter((a) => a.id != id)]);
+    });
   };
 
   useEffect(() => {
     agent.Activities.list().then((response) => {
       let activities: IActivity[] = [];
       response.forEach((activity) => {
-        activity.date = activity.date.split('.')[0];
+        activity.date = activity.date.split(".")[0];
         activities.push(activity);
       });
       setActivities(activities);
@@ -53,7 +55,7 @@ const App = () => {
   return (
     <Fragment>
       <NavBar openCreateForm={handleOpenCreateForm} />
-      <Container style={{ marginTop: '7em' }}>
+      <Container style={{ marginTop: "7em" }}>
         <ActivityDashboard
           activities={activities}
           selectActivity={handleSelectedActivity}
