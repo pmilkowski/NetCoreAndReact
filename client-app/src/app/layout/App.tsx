1e4bb89e66b31dac5c5477043ed58e4d24621a0e
Pawel Milkowski <milkowskipaw@gmail.com>
2020-10-16 16:08:05 +0200

    Added reading initial activities from store

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -55,25 +55,17 @@ const App = () => {
   };
 
   useEffect(() => {
-    agent.Activities.list().then((response) => {
-      let activities: IActivity[] = [];
-      response.forEach((activity) => {
-        activity.date = activity.date.split(".")[0];
-        activities.push(activity);
-      });
-      setActivities(activities);
-    }).then(() => setLoading(false));
-  }, []);
+    activityStore.loadActivities();
+  }, [activityStore]);
 
-  if (loading) return <LoadingComponent content="Loading activities..." />
+  if (activityStore.loadingInitial) return <LoadingComponent content="Loading activities..." />
 
   return (
     <Fragment>
       <NavBar openCreateForm={handleOpenCreateForm} />
       <Container style={{ marginTop: "7em" }}>
-        <h1>{activityStore.title}</h1>
         <ActivityDashboard
-          activities={activities}
+          activities={activityStore.activities}
           selectActivity={handleSelectedActivity}
           selectedActivity={selectedActivity}
           setSelectedActivity={setSelectedActivity}
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,8 +1,24 @@
-import {observable} from 'mobx';
-import { createContext } from 'react';
+import { action, observable } from "mobx";
+import { createContext } from "react";
+import agent from "../api/agent";
+import { IActivity } from "../models/activity";
 
 class ActivityStore {
-    @observable title = 'Hello from Mobx';
+  @observable activities: IActivity[] = [];
+  @observable loadingInitial = false;
+
+  @action loadActivities = () => {
+    this.loadingInitial = true;
+
+    agent.Activities.list()
+      .then((activities) => {
+        activities.forEach((activity) => {
+          activity.date = activity.date.split(".")[0];
+          this.activities.push(activity);
+        });
+      })
+      .finally(() => (this.loadingInitial = false));
+  };
 }
 
-export default createContext(new ActivityStore())
+export default createContext(new ActivityStore());
