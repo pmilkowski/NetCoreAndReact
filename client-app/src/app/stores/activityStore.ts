b24b802652a134cad0441023801f62f604a22e8d
Pawel Milkowski <milkowskipaw@gmail.com>
2020-10-16 18:22:41 +0200

    Added selectActivity to store

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -6,6 +6,8 @@ import { IActivity } from "../models/activity";
 class ActivityStore {
   @observable activities: IActivity[] = [];
   @observable loadingInitial = false;
+  @observable selectedActivity: IActivity | undefined;
+  @observable editMode = false;
 
   @action loadActivities = () => {
     this.loadingInitial = true;
@@ -18,6 +20,13 @@ class ActivityStore {
         });
       })
       .finally(() => (this.loadingInitial = false));
+  };
+
+  @action selectActivity = (id: string) => {
+    this.selectedActivity = this.activities.find(
+      (activity) => activity.id === id
+    );
+    this.editMode = false;
   };
 }
 
