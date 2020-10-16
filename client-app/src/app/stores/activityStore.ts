5853640eef8c22678422ccd8760a386902b0c828
Pawel Milkowski <milkowskipaw@gmail.com>
2020-10-16 19:17:08 +0200

    Enabled AutoObservable on ActivityStore

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,13 +1,17 @@
-import { action, observable } from "mobx";
+import { action, makeAutoObservable } from "mobx";
 import { createContext } from "react";
 import agent from "../api/agent";
 import { IActivity } from "../models/activity";
 
 class ActivityStore {
-  @observable activities: IActivity[] = [];
-  @observable loadingInitial = false;
-  @observable selectedActivity: IActivity | undefined;
-  @observable editMode = false;
+  activities: IActivity[] = [];
+  loadingInitial = false;
+  selectedActivity: IActivity | undefined;
+  editMode = false;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
 
   @action loadActivities = () => {
     this.loadingInitial = true;
