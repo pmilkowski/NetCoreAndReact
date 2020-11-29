f3e3af5a30d1d96d54ad9096e7febd5caabb59f6
Pawel Milkowski <milkowskipaw@gmail.com>
2020-11-29 15:35:00 +0100

    Added React Router Dom

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
-
+import { BrowserRouter } from 'react-router-dom'
 ReactDOM.render(
     <App />,
     document.getElementById('root')
